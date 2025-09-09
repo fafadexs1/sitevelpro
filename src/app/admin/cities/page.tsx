b4c4e5b039fca966964483dc0bb619584d107701
Fabricio de Sousa Soares <fabriciobb09@gmail.com>
2025-09-09 04:11:31 +0000

    ReferenceError: AlertDialogFooter is not defined     at https://6000-fir

--- src/app/admin/cities/page.tsx
+++ src/app/admin/cities/page.tsx
@@ -39,6 +39,7 @@ import {
     AlertDialogCancel,
     AlertDialogContent,
     AlertDialogDescription,
+    AlertDialogFooter,
     AlertDialogHeader,
     AlertDialogTitle,
     AlertDialogTrigger,
