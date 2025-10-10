d40e185993007243007036d3c85249048cd9864a
Fabricio de Sousa Soares <fabriciobb09@gmail.com>
2025-10-10 20:45:53 +0000

    https://bqxdyinyzfxqghyuzsbs.supabase.co/storage/v1/object/public/site-a

--- src/components/landing/Streaming.tsx
+++ src/components/landing/Streaming.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import Image from "next/image";
@@ -8,21 +7,21 @@ import { motion, AnimatePresence } from "framer-motion";
 const streamingServices = [
   {
     bgImage: "https://s3.glbimg.com/v1/AUTH_c7def7ff66fe4b13beac0810ffbf780f/secure/now-at-globo-share-d5539fc.jpg",
-    logo: "https://velpro.net.br/wp-content/uploads/2025/06/48D087-1.png",
+    logo: "https://bqxdyinyzfxqghyuzsbs.supabase.co/storage/v1/object/public/site-assets/Streamings/Globoplay.webp",
     logoAlt: "Globoplay",
     title: "Velpro + Globoplay, isso é entretenimento",
     description: "A diversão no entretenimento, a informação do jornalismo e muita emoção nos esportes.",
   },
   {
     bgImage: "https://i0.wp.com/cloud.estacaonerd.com/wp-content/uploads/2019/10/06213603/telecine.jpg?fit=1920%2C1080&ssl=1",
-    logo: "https://velpro.net.br/wp-content/uploads/2025/06/48D087-2.png",
+    logo: "https://bqxdyinyzfxqghyuzsbs.supabase.co/storage/v1/object/public/site-assets/Streamings/Telecine.webp",
     logoAlt: "Telecine",
     title: "Tenha os maiores sucessos do cinema com a Velpro + Telecine",
     description: "Assista aos maiores lançamentos de Hollywood e clássicos do cinema com qualidade e exclusividade.",
   },
   {
     bgImage: "https://www.claro.com.br/files/104379/1920x600/6eeab4eba7/img-banner-custom-brasileirao-2025-premiere-desk.jpg?sq=75",
-    logo: "https://velpro.net.br/wp-content/uploads/2025/06/48D087.jpg",
+    logo: "https://bqxdyinyzfxqghyuzsbs.supabase.co/storage/v1/object/public/site-assets/Streamings/Premiere.webp",
     logoAlt: "Premiere",
     title: "Futebol ao vivo é com a Velpro + Premiere",
     description: "Acompanhe seu time do coração em todos os jogos do Brasileirão com emoção e transmissão ao vivo.",
